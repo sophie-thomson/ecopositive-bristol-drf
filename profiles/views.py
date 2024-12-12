b00af27fef4cb7a44386f3bb3527cc00de967574
Sophie Thomson <sophielbenoit@gmail.com>
2024-12-12 22:56:03 +0000

    Add filtering by filtersets to provile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -15,8 +16,21 @@ class ProfileList(generics.ListAPIView):
     ).order_by('-created_on')
     serializer_class = ProfileSerializer
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
     ]
+
+    filterset_fields = [
+        # filter by individual profile
+        'owner',
+    ]
+
+    search_fields = [
+        'first_name',
+        'last_name',
+    ]
+
     ordering_fields = [
         'endorsements_count',
         'owner__endorsing_user__created_on',
