f68d289033bd29c8078503b649713caab6138f81
Sophie Thomson <sophielbenoit@gmail.com>
2025-01-11 11:01:42 +0000

    Fix: Add image validation to profile serializers to restict image size and improve performance

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -8,6 +8,21 @@ class ProfileSerializer(serializers.ModelSerializer):
     endorsements_count = serializers.ReadOnlyField()
     is_staff = serializers.SerializerMethodField()
 
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size too large, please use an image smaller than 2MB'
+            )
+        if value.image.width > 1000:
+            raise serializers.ValidationError(
+                'Image width must be less than 1000px'
+            )
+        if value.image.height > 1000:
+            raise serializers.ValidationError(
+                'Image height must be less than 1000px'
+            )
+        return value
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
